180fb6e31cb6726879b28f2d376f6d394fed377f
SeguierJonathan <jonathan.seguier@oclock.school>
2026-06-03 19:14:36 +0200

    chore(deps): add cors

--- backend-node-api/index.js
+++ backend-node-api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import { sequelize } from "./models/index.js";
+import cors from "cors"
 import routerList from "./router/list.routes.js";
 import routerCard from "./router/card.routes.js";
 import routerTag from "./router/tag.routes.js";
@@ -8,6 +9,8 @@ import routerTag from "./router/tag.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3310;
+
+app.use(cors());
 
 app.use('/lists', routerList);
 app.use('/cards', routerCard);
