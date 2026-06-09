e07247ba342eb1d603f96942b615d73ff69d6ea2
SeguierJonathan <jonathan.seguier@oclock.school>
2026-06-09 17:42:29 +0200

    chore: rename backend-go directory

---
+++ backend-go/cmd/api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+type Board struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "helloworld")
+}
+
+func getAll(w http.ResponseWriter, r *http.Request) {
+
+	board := Board{Id: 1, Name: "je suis name"}
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(board)
+}
+
+func main() {
+
+	http.HandleFunc("/", hello)
+
+	http.HandleFunc("/boards", getAll)
+
+	port := ":5005"
+
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println("Port already in use:", err)
+		return
+	}
+	ln.Close()
+
+	fmt.Println("Server start at: http://localhost" + port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
+
+}
